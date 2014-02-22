91b1ff84803ce73f88073eb6c13ef94e9759ceaa
Jag Gunawardana <jag.gunawardana@workbox>
2014-02-22 18:53:19 +0000

    Updated packges.

---
+++ packages.sh
@@ -0,0 +1,8 @@
+sudo apt-get install git python-pip
+sudo apt-get install mysql-server
+sudo apt-get install mysql-client
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install mysql-client
+sudo apt-get install python-dev
+sudo pip install virtualenv==1.10.1
+sudo pip install virtualenvwrapper
