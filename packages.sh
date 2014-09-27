e50865ed545b814dd17c8dfe6195e143e487f6a0
Jag Gunawardana <jag.gunawardana@saffrondigital.com>
2014-09-27 22:03:49 +0000

    new packages

--- packages.sh
+++ packages.sh
@@ -7,11 +7,17 @@ sudo apt-get install -y postgresql
 sudo apt-get install -y postgresql-contrib
 sudo apt-get install -y mysql-client
 sudo apt-get install -y python-dev
+sudo apt-get install -y tmux
+sudo apt-get install -y vim-athena
+sudo apt-get install -y g++
+sudo apt-get install -y exuberant-ctags
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
 sudo pip install pytest
 sudo pip install ipython
 sudo pip install ipdb
+sudo pip install coverage
+echo trustybox > /etc/hostname
 
 mkdir -p /home/vagrant/bin
 
@@ -33,6 +39,7 @@ ln -s `pwd`/bin/file_watch.sh ~/bin/file_watch.sh
 ln -s `pwd`/.pdbrc.py ~/.pdbrc.py
 ln -s `pwd`/.Renviron ~/.Renviron
 ln -s `pwd`/.Rprofile ~/.Rprofile
+ln -s /home/vagrant/mac/bin/db.sh ~/bin/db.sh
 popd
 
 mkdir /home/vagrant/.vim
@@ -40,3 +47,5 @@ mkdir /home/vagrant/.vim/bundle
 git clone https://github.com/gmarik/vundle.git /home/vagrant/.vim/bundle/vundle
 vim +PluginInstall +qall
 pip install git+git://github.com/mverteuil/pytest-ipdb.git
+git config --global user.name "Jag Gunawardana"
+git config --global user.email "jag.gunawardana@saffrondigital.com"
