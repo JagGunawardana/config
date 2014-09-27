e995eb3d39398b1028c86c9a3f9aba1e920af1de
Jag Gunawardana <jag.gunawardana@saffrondigital.com>
2014-09-27 11:14:14 +0000

    updates

--- packages.sh
+++ packages.sh
@@ -1,28 +1,30 @@
-sudo apt-get install -y git python-pip
+sudo apt-get install -y git
+sudo apt-get install -y python-pip
 sudo apt-get install -y mysql-server
 sudo apt-get install -y mysql-client
 sudo apt-get install -y libmysqlclient-dev
+sudo apt-get install -y postgresql
+sudo apt-get install -y postgresql-contrib
 sudo apt-get install -y mysql-client
 sudo apt-get install -y python-dev
-sudo apt-get install -y python-pip
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
 sudo pip install pytest
 sudo pip install ipython
 sudo pip install ipdb
 
-mkdir -p ~/bin
+mkdir -p /home/vagrant/bin
 
 
-rm ~/.vimrc
-rm ~/.tmux.conf
-rm ~/.bashrc
-rm ~/bin/file_watch.sh
-rm ~/.pdbrc.py
-rm ~/.Renviron
-rm ~/.Rprofile
+rm /home/vagrant/.vimrc
+rm /home/vagrant/.tmux.conf
+rm /home/vagrant/.bashrc
+rm /home/vagrant/bin/file_watch.sh
+rm /home/vagrant/.pdbrc.py
+rm /home/vagrant/.Renviron
+rm /home/vagrant/.Rprofile
 
-pushd ~/setup/config
+pushd /home/vagrant/config
 ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.bashrc  ~/.bashrc
@@ -33,8 +35,8 @@ ln -s `pwd`/.Renviron ~/.Renviron
 ln -s `pwd`/.Rprofile ~/.Rprofile
 popd
 
-mkdir ~/.vim
-mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+mkdir /home/vagrant/.vim
+mkdir /home/vagrant/.vim/bundle
+git clone https://github.com/gmarik/vundle.git /home/vagrant/.vim/bundle/vundle
 vim +PluginInstall +qall
 pip install git+git://github.com/mverteuil/pytest-ipdb.git
